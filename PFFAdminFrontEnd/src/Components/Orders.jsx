f602b5b030e41d8ba8fc3b9b2095ff7b39bf3586
Himesh-Kukadiya <himeshkukadiya075@gmail.com>
2024-12-04 14:47:21 +0530

    Added seargin function to filter on search

--- PFFAdminFrontEnd/src/Components/Orders.jsx
+++ PFFAdminFrontEnd/src/Components/Orders.jsx
@@ -5,7 +5,12 @@ const Orders = () => {
     const [refresh, setRefresh] = useState(false);
     const [rents, setRents] = useState([]);
     const [filteredRents, setFilteredRents] = useState([]);
+    const [detailedFilterRents, setDetailedFilterRents] = useState([]);
     const [dates, setDates] = useState({});
+
+    useEffect(() => {
+        setDetailedFilterRents(filteredRents)
+    }, [filteredRents])
 
     useEffect(() => {
         axios.get('http://localhost:7575/api/admin/getRents')
@@ -71,10 +76,34 @@ const Orders = () => {
         return filteredData;
     };
 
+    const handleFilterOnStatus = (e) => {
+        const filterdData = filteredRents.filter(rent => rent.status === e.target.value)
+        setDetailedFilterRents(filterdData);
+    }
+
+    const handleSearchRents = (e) => {
+        const query = e.target.value.toLowerCase(); // Get the search query and convert to lowercase
+        const searchResult = filteredRents.filter((rent) => {
+            // Check for matches in rentId, userId, mobile, or within products (productId, productName)
+            const matchesRentId = rent.id.toString().toLowerCase().includes(query);
+            const matchesUserId = rent.uId.toString().toLowerCase().includes(query);
+            const matchesMobile = rent.mobile.includes(query);
+            const matchesProduct = rent.products.some((product) => {
+                const matchesProductId = product.id.toString().toLowerCase().includes(query);
+                const matchesProductName = product.name.toLowerCase().includes(query);
+                return matchesProductId || matchesProductName;
+            });
+    
+            return matchesRentId || matchesUserId || matchesMobile || matchesProduct;
+        });
+        setDetailedFilterRents(searchResult);
+    }
     return (
         <section id="product" className="products-section">
             <div className="container-fluid">
                 <div className="d-flex justify-content-start mb-4">
+
+                    {/* filter by time */}
                     <select className="dropdown btn btn-light dropdown-toggle" name="filterOnTime" onChange={handleFilterOnTime} id="">
                         <option className="dropdown-item" value="">Select Time</option>
                         <option className="dropdown-item" value="today">Today</option>
@@ -83,19 +112,30 @@ const Orders = () => {
                         <option className="dropdown-item" value="year">This Year</option>
                         <option className="dropdown-item" value="all">All Rents</option>
                     </select>
+                    {/* filter by status */}
+                    <select className="dropdown btn btn-light dropdown-toggle ml-3" name="filterOnTime" onChange={handleFilterOnStatus} id="">
+                        <option className="dropdown-item" value="">Select Status</option>
+                        <option className="dropdown-item" value="Accepted">Accepted</option>
+                        <option className="dropdown-item" value="Pending">Pending</option>
+                        <option className="dropdown-item" value="Rejected">Rejected</option>
+                    </select>
+                    {/* filter by specific date */}
+                    <div className="ml-3 d-flex align-items-center justify-content-center border rounded py-1 px-2">
+                        <input type="date" name="from" className="form-control text-light bg-transparent"
+                        onChange={(e)=> {setDates({...dates, from: e.target.value})}}/>
 
-                    <div className="ml-3 d-flex align-items-center justify-content-center">
-                            <label className="font-weight-bold">From: </label>
-                            <input type="date" name="from" className="form-control text-light bg-transparent ml-2"
-                            onChange={(e)=> {setDates({...dates, from: e.target.value})}}/>
+                        <label className="font-weight-bold ml-2 pt-2">To </label>
+                        <input type="date" name="to" className="form-control text-light bg-transparent ml-2"
+                        onChange={(e)=> {setDates({...dates, to: e.target.value})}}/>
 
-                            <label className="font-weight-bold ml-2">To: </label>
-                            <input type="date" name="to" className="form-control text-light bg-transparent ml-2"
-                            onChange={(e)=> {setDates({...dates, to: e.target.value})}}/>
-
-                            <button className="btn btn-light ml-2"
-                            onClick={() => {setFilteredRents(filterData(dates.from, dates.to))}}> <BsSearch /></button>
+                        <button className="btn btn-light ml-2"
+                        onClick={() => {setFilteredRents(filterData(dates.from, dates.to))}}> <BsSearch /></button>
                     </div>
+                    {/* filter by searching */}
+                    <div className="ml-3 d-flex align-items-center justify-content-center border rounded py-1 px-2 w-75">
+                        <input type="text" className="form-control bg-transparent text-light" placeholder="id, pId, uId, pId, pName, Mobile" onChange={handleSearchRents} />
+                    </div>
+
                 </div>
                 <div className="table-container" style={{ overflowX: "auto" }}>
                     <table className="table table-striped text-light table-hover table-dark table-bordered">
@@ -111,8 +151,8 @@ const Orders = () => {
                             </tr>
                         </thead>
                         <tbody className="table-body">
-                            {filteredRents.length > 0 
-                            ? filteredRents.map((rent, index) => (
+                            {detailedFilterRents.length > 0 
+                            ? detailedFilterRents.map((rent, index) => (
                                 <tr key={rent.id + index}>
                                     <td className="text-center">{index + 1}</td>
                                     <td className="text-left">
